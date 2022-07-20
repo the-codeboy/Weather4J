f2da6092b3cc702ce0e5d4d4d28ca47bb58c84c9
the_codeboy <71213855+the-codeboy@users.noreply.github.com>
2022-07-20 09:43:12 +0200

    Fix bug with workaround

--- src/main/java/ml/codeboy/met/data/ApiResponse.java
+++ src/main/java/ml/codeboy/met/data/ApiResponse.java
@@ -17,18 +17,22 @@ public class ApiResponse {
         JsonArray timeseries = element.getAsJsonObject().get("properties").getAsJsonObject().get("timeseries").getAsJsonArray();
 
         for (JsonElement elem : timeseries) {
-            JsonObject forecast = elem.getAsJsonObject();
-            Instant time = Instant.parse(forecast.get("time").getAsString());
+            try {
+                JsonObject forecast = elem.getAsJsonObject();
+                Instant time = Instant.parse(forecast.get("time").getAsString());
 
-            JsonObject data = forecast.get("data").getAsJsonObject();
-            JsonObject details = data.get("instant").getAsJsonObject().get("details").getAsJsonObject();
-            double airTemperature = details.get("air_temperature").getAsDouble();
-            double humidity = details.get("relative_humidity").getAsDouble();
-            double windSpeed = details.get("wind_speed").getAsDouble();
-            String symbol = data.get("next_1_hours").getAsJsonObject().get("summary").getAsJsonObject().get("symbol_code").getAsString();
+                JsonObject data = forecast.get("data").getAsJsonObject();
+                JsonObject details = data.get("instant").getAsJsonObject().get("details").getAsJsonObject();
+                double airTemperature = details.get("air_temperature").getAsDouble();
+                double humidity = details.get("relative_humidity").getAsDouble();
+                double windSpeed = details.get("wind_speed").getAsDouble();
+                String symbol = data.get("next_1_hours").getAsJsonObject().get("summary").getAsJsonObject().get("symbol_code").getAsString();
 
-            Forecast result = new Forecast(time, airTemperature, humidity, windSpeed, symbol);
-            forecasts.add(result);
+                Forecast result = new Forecast(time, airTemperature, humidity, windSpeed, symbol);
+                forecasts.add(result);
+            } catch (Exception e) {
+                //workaround for now
+            }
         }
     }
 
